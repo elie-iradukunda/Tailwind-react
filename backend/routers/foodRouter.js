a0aaa7285edf87a2549999adddec3fdfce7145f1
elieiradukunda <iradukundaelie71@gmail.com>
2025-08-04 15:29:09 +0200

    displaying list of foods added

--- backend/routers/foodRouter.js
+++ backend/routers/foodRouter.js
@@ -14,6 +14,24 @@ router.get('/getallFood', async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 });
+router.post('/addfood',async(req,res)=>{
+        
+        const {title,description}=req.body
+    try {
+
+        const checkingFoodQuery=await pool.query(`select * from foods where  title=$1`,[title])
+
+        if(checkingFoodQuery.rowCount>0){
+            res.status(404).json({message:"that food type allready added"})
+        }
+    const insertQuery=await pool.query(`insert into foods(title,description) values($1,$2) RETURNING *`,[title,description])
+    res.status(200).json(insertQuery.rows[0])
+        
+    } catch (err) {
+        console.error(err);
+    res.status(500).json({ message: 'Server error' });
+    }
+})
 
 
 export default router
